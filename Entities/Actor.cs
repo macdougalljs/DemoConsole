b91ce3349d65a561f0ad155f4b2ee166c2666b57
Jeremy MacDougall <macdojer@outlook.com>
2022-05-02 22:21:00 -0400

    Add project files.

---
+++ Entities/Actor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoConsole.Entities
+{
+    internal class Actor
+    {
+        public int Id
+        {
+            get; set;
+        }
+
+        public string Name
+        {
+            get; set;
+        }
+
+        public int Age
+        {
+            get; set;
+        }
+
+        public bool AcademyWinner
+        {
+            get; set;
+        }
+    }
+}
---
+++ Entities/ActorDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DemoConsole.Entities
+{
+    internal class ActorDbContext : DbContext
+    {
+        public DbSet<Actor> Actors
+        {
+            get; set;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=TestActorDb;"
+            + "Trusted_Connection=true;");  // should go into a connection string in configuration not hardcoded
+        }
+    }
+}
