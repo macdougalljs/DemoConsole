963ed356935bfdf4c6a0495d5c5ef5c9d7e1ced0
Jeremy MacDougall <macdojer@outlook.com>
2022-05-02 22:34:58 -0400

    Create Entities, Create DbContext, Migrations, Seed and run app

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DemoConsole.Entities;
+using System;
 
 namespace DemoConsole
 {
@@ -6,7 +7,36 @@ namespace DemoConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var db = new ActorDbContext())
+            {
+                // Seed
+                db.Actors.AddRange(new Actor
+                {
+                    Name = "Bruce Lee",
+                    Age = 75,
+                    AcademyWinner = false
+                },
+               new Actor
+               {
+                   Name = "Madonna",
+                   Age = 55,
+                   AcademyWinner = true
+               });
+
+                var count = db.SaveChanges();
+
+                // Fetch data and & write it out
+
+                Console.WriteLine($"{count} records added");
+
+                foreach (var Actor in db.Actors)
+                {
+                    Console.WriteLine($"Name - {Actor.Name},\t\t" +
+                        $"Age: {Actor.Age},\t\t" +
+                        $"Academy Winner: - {Actor.AcademyWinner}");
+                }
+                Console.ReadLine();
+            }
         }
     }
 }
